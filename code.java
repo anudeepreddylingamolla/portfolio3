d81b7299ac238da7706deaf6306a2fc1733a2354
anudeepreddylingamolla <114795698+anudeepreddylingamolla@users.noreply.github.com>
2023-02-19 20:57:46 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,28 @@
+@import url('https://fonts.googleapis.com/css2?family=Poor+Story&family=Roboto:ital,wght@1,100;1,700&display=swap');
+.anu {
+    background-image: url(about.jpg);
+    background-position: center;
+    background-attachment: fixed;
+   
+
+}
+.navbar .logo {
+    width: 4cm;
+}
+.na {
+    background-color: aqua;
+}
+.form {
+    display: inline;
+}
+.fa {
+    padding: 20px;
+    font-size: 30px;
+    width: 30px;
+    text-align: center;
+    text-decoration: none;
+    border-radius: 50%;
+  }
+ .la {
+    text-align: center;
+ }
